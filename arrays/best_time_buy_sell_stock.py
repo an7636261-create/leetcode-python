411e76dbededba4584ba8ce88e004a3b84fbff91
Aksh217 <nakshaya412@gmail.com>
2026-01-12 23:00:38 +0530

    Reorganized project folder, added new files

---
+++ arrays/best_time_buy_sell_stock.py
@@ -0,0 +1,14 @@
+def maxprofit(self,prices):
+    min_price=float('inf')
+    max_profit=0
+
+    for price in prices:
+        if price < min_price:
+            min_price = price
+        else:
+            profit = price - min_price
+            max_profit = max(max_profit,profit)
+
+    return max_profit
+
+print(maxProfit([7,1,5,3,6,4]))
