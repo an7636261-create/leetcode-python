2629b10897b52cc81e67eaca214cdad84ba31702
Aksh217 <nakshaya412@gmail.com>
2026-01-23 15:10:09 +0530

    Solved Maximum subarray

---
+++ arrays/maximum_avg_subarray.py
@@ -0,0 +1,14 @@
+def findMaxAverage(nums,k):
+    window_sum=sum(nums[:k])
+    max_sum=window_sum
+
+    for i in range(k,len(nums)):
+        window_sum += nums[i]
+        window_sum -= nums[i-k]
+        max_sum = max(max_sum,window_sum)
+
+    return max_sum/float(k)
+
+nums=[1,12,-5,-6,50,3]
+result=findMaxAverage(nums,4)
+print("Maximum average is:",result)
