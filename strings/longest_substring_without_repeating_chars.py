b732b28bde0813b03d56f91beb2d76c485a71cdd
Aksh217 <nakshaya412@gmail.com>
2026-01-26 21:46:28 +0530

    Solved logest sunstring problem

---
+++ strings/longest_substring_without_repeating_chars.py
@@ -0,0 +1,16 @@
+def lengthOfLongestSubstring(s):
+    char_set = set()
+    left = 0
+    max_len = 0
+
+    for right in range(len(s)):
+        while s[right] in char_set:
+            char_set.remove(s[left]) #remove left duplicate char
+            left+=1 #and move forward
+        char_set.add(s[right]) #now add right char(unique)
+        max_len=max(max_len,right-left+1) #(right-left+1)is the current max length
+
+    return max_len
+
+s="abcabcbb"
+print("Result",lengthOfLongestSubstring(s))
