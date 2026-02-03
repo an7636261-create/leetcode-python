4f5564a62a738b5915f61ba361043abfc5950bde
Aksh217 <nakshaya412@gmail.com>
2026-02-03 22:14:56 +0530

    Added next_greater1 fie

---
+++ Stacks/next_greater1.py
@@ -0,0 +1,23 @@
+def nextgreater(nums1,nums2):
+    stack = []
+    next_greater = {} #dictionary (hash map)
+    
+    #process nums2
+    for num in nums2:
+        while stack and num > stack[-1]:
+            prev = stack.pop()
+            next_greater[prev] = num
+        stack.append(num)
+        
+    #elements ;eft in stack have no next greater
+    for num in stack:
+        next_greater[num] = -1
+
+    #build result for nums1 
+    return[next_greater[num] for num in nums1]
+
+#example
+nums1 = [4,1,2] 
+nums2 = [1,3,4,2]
+result = nextgreater(nums1, nums2)
+print(result)
